4ca86cc55b0b96a21c7564fe9f7e17ff81654d9d
Majdoub-Rayen <116362274+Majdoub-Rayen@users.noreply.github.com>
2025-03-04 11:29:58 +0100

    Add files via upload

--- gestionPaiement/Service/WalletService.java
+++ gestionPaiement/Service/WalletService.java
@@ -4,6 +4,9 @@ import com.example.freelanci.gestionPaiement.Entity.Wallet;
 import com.example.freelanci.gestionPaiement.Repository.WalletRepository;
 import com.example.freelanci.gestionUser.entities.User;
 import com.example.freelanci.gestionUser.repositories.UserRepository;
+import com.twilio.Twilio;
+import com.twilio.rest.api.v2010.account.Message;
+import com.twilio.type.PhoneNumber;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -34,6 +37,9 @@ public class WalletService {
             wallet.setAmount(amount);
             wallet.setDate(LocalDate.now());
             wallet.setTime(LocalTime.now());
+            String recipientNumber = "+216"+93701055;
+            String messageBody = "Dear " + user.getUsername() + ",\nYour deposit of " + amount + " TND has been successfully completed.\nCurrent Balance : " + user.getAccountBalance() + " TND.\nThank you for your trust!";
+            sendSMS(recipientNumber, messageBody);
 
 
             return walletRepository.save(wallet);
@@ -53,6 +59,9 @@ public class WalletService {
                 wallet.setAmount(amount);
                 wallet.setDate(LocalDate.now());
                 wallet.setTime(LocalTime.now());
+                String recipientNumber = "+21693701055";
+                String messageBody = "Dear " + user.getUsername() + ",\nYour withdrawal of " + amount + " TND has been successfully completed.\nCurrent Balance : " + user.getAccountBalance() + " TND.\nThank you for your loyalty!";
+                sendSMS(recipientNumber, messageBody);
 
 
                 return walletRepository.save(wallet);
@@ -71,5 +80,22 @@ public class WalletService {
         User user = userRepository.findById(userId)
                 .orElseThrow(() -> new RuntimeException("User not found"));
         return walletRepository.findByUser(user);
+    }
+    private static final String ACCOUNT_SID = "ACbb0e8297898b6c4cc1ad0dd8ba356bb4";
+    private static final String AUTH_TOKEN = "3ace996fee64fab83c6d0b06fa8cf836";
+
+    public void sendSMS(String recipientNumber, String messageBody) {
+        try {
+            Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
+            Message message = Message.creator(
+                            new PhoneNumber(recipientNumber),
+                            new PhoneNumber("+14638420119"),
+                            messageBody)
+                    .create();
+
+            System.out.println("SMS sent successfully. SID: " + message.getSid());
+        } catch (Exception e) {
+            System.err.println("Erreur lors de l'envoi du SMS : " + e.getMessage());
+        }
     }
 }
